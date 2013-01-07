0ae5331ea9a115723054ef2839a0159d531edce8
pmeinhardt <paul.meinhardt@student.hpi.uni-potsdam.de>
2013-01-07 14:35:07 +0100

    Initial commit w/ looped sound and animated image

--- Hypnotoad/ViewController.h
+++ Hypnotoad/ViewController.h
@@ -7,7 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController : UIViewController
+
+@property (nonatomic, retain) IBOutlet UIImageView *imageView;
+
+@property (nonatomic, retain) AVAudioPlayer *audio;
 
 @end
--- Hypnotoad/ViewController.m
+++ Hypnotoad/ViewController.m
@@ -14,16 +14,57 @@
 
 @implementation ViewController
 
+@synthesize imageView=_imageView;
+@synthesize audio=_audio;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    NSArray *images = [NSArray arrayWithObjects:
+                       [UIImage imageNamed:@"toad-0.png"],
+                       [UIImage imageNamed:@"toad-1.png"],
+                       [UIImage imageNamed:@"toad-2.png"],
+                       [UIImage imageNamed:@"toad-3.png"],
+                       [UIImage imageNamed:@"toad-4.png"],
+                       [UIImage imageNamed:@"toad-5.png"], nil];
+
+    [self.imageView setAnimationImages:images];
+    [self.imageView setAnimationDuration:0.4];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self.imageView startAnimating];
+
+    if (self.audio == nil) {
+        NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"hypno" ofType:@"m4a"]];
+        self.audio = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:NULL];
+        [self.audio setNumberOfLoops:-1];
+    }
+
+    [self.audio play];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [self.audio stop];
+    [self.imageView stopAnimating];
+    [super viewWillDisappear:animated];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc
+{
+    [_imageView release];
+    [_audio release];
+    [super dealloc];
 }
 
 @end
